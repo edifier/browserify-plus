7d8441cb240d73096ecc5d4f01b93d67b3d0837d
edifier <edifier1988216@163.com>
2015-10-15 16:07:19 +0800

    npm test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 /**
  * Created by wangxin8 on 2015/10/8.
- * 测试文件，终端：node test.js
+ * 测试文件，终端：npm test
  */
 
 'use strict';
@@ -8,19 +8,19 @@ var browserifyPlus = require('../index.js');
 
 var config = {
     //需要编译的文件夹
-    inputPath: './src/',
+    inputPath: './test/src/',
     output: {
         //输出banner
-        banner:'/*build by wangxin at <%time%>*/\n',
+        banner:'/*build at <%time%>*/\n',
         //输出文件路径
-        path: '../js/',
+        path: './js/',
         //输出方式: normal、deep
         type: 'normal',
         //是否压缩
         compress: true
     },
     //引用的库文件路径
-    libraryPath: '../core/',
+    libraryPath: './core/',
     watch: {
         //watch轮询的时常，默认值1200
         interval: 1000
